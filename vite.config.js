75f3bc9b984613443575a79b37ac624604579ec0
Himansh1975 <himanshnegi2001@gmail.com>
2024-08-31 23:49:50 +0530

    deploy ready

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<REPO_NAME>/', // Replace <REPO_NAME> with your GitHub repository name
+  base: '/venue-hub/', // Replace <REPO_NAME> with your GitHub repository name
 })
